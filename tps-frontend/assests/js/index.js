c9dc5fde71a2af9b240cf5048dafef7294c6cb24
shamodhasahan <Shamodha2729@>
2023-10-24 22:24:55 +0530

    updating

--- tps-frontend/assests/js/index.js
+++ tps-frontend/assests/js/index.js
@@ -1,29 +1,21 @@
-document.addEventListener("DOMContentLoaded", function (event) {
 
+document.addEventListener("DOMContentLoaded", function (event) {
+    navigate('dashboard')
     const showNavbar = (toggleId, navId, bodyId, headerId) => {
         const toggle = document.getElementById(toggleId),
             nav = document.getElementById(navId),
             bodypd = document.getElementById(bodyId),
             headerpd = document.getElementById(headerId)
-
-        // Validate that all variables exist
         if (toggle && nav && bodypd && headerpd) {
             toggle.addEventListener('click', () => {
-                // show navbar
                 nav.classList.toggle('show')
-                // change icon
                 toggle.classList.toggle('bx-x')
-                // add padding to body
                 bodypd.classList.toggle('body-pd')
-                // add padding to header
                 headerpd.classList.toggle('body-pd')
             })
         }
     }
-
     showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
-
-    /*===== LINK ACTIVE =====*/
     const linkColor = document.querySelectorAll('.nav_link')
 
     function colorLink() {
@@ -33,6 +25,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     }
     linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-    // Your code to run since DOM is loaded and ready
 });
+
+$('.nav .nav_link').click(event => {
+    event.preventDefault(); // Prevent the link from navigating
+    var text = $(event.currentTarget).find('.nav_name').text();
+    $('#header-text').text(text);
+    navigate(text);
+})
+
+const navigate = (text)=>{
+    $('.body-container').css('display','none');
+    $('#'+text.toLowerCase()+'-section').css("display", "block");
+}
